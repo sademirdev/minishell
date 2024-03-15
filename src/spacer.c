427251d3abfbdb93f880b08a647c51d4fda950b6
Samet Demir <sademir@k1m08s09.42istanbul.com.tr>
2024-03-15 13:06:30 +0300

    recursive updated

--- src/spacer.c
+++ src/spacer.c
@@ -2,14 +2,10 @@
 #include <stdint.h>
 
 static int64_t	separated_list_len(char *prompt);
-void	rucursive(char *prompt, int64_t i, int64_t flag);
-
+static int64_t	recursive(char *prompt, int64_t *i, int64_t *flag);
 
 char	**separate_prompt_by_space(char *prompt)
 {
-	bool	flag;
-
-
 	return 0;
 }
 
@@ -31,100 +27,71 @@ static int64_t	separated_list_len(char *prompt)
 			while (prompt[i] == ' ')
 				i++;
 			if (!prompt[i])
-				break;
+				break ;
 			count++;
-			while (prompt[i] != ' ' && prompt[i])
+			while (prompt[i] && prompt[i] != ' ')
 			{
-				if (!flag && prompt[i] == '\'')
+				if (prompt[i] == '\'')
 				{
 					flag = prompt[i];
 					i++;
-					while (prompt[i] && prompt[i] != '\'')
-						i++;
-					if (flag && prompt[i] == '\'')
-					{
-						flag = 0;
-						if (prompt[i + 1] != ' ' && prompt[i + 1] != '\'')
-							while (prompt[i] && prompt[i] != ' ')
-								i++;
-					}
+					if (recursive(prompt, &i, &flag) == -42)
+						return (-42);
 				}
 				i++;
 			}
 		}
-		if (!flag && prompt[i] == '"')
+		if (!flag && prompt[i] == '\'')
 		{
 			flag = prompt[i];
 			count++;
 			i++;
-			while (prompt[i] && prompt[i] != '"')
-				i++;
-			if (prompt[i] == '"')
-				flag = 0;
+			if (recursive(prompt, &i, &flag) == -42)
+				return (-42);
 			if (prompt[i])
 				i++;
-			// if (flag)
-			// 	return (-41);
-		}
-		else if (!flag && prompt[i] == '\'')
-		{
-			flag = prompt[i];
-			count++;
-			i++;
-			rucursive(prompt, i, flag);
-			// while (prompt[i] && prompt[i] != '\'')
-			// 	i++;
-			// if (prompt[i] == '\'')
-			// {
-			// 	flag = 0;
-			// 	if (prompt[i + 1] != ' ' && prompt[i + 1] != '\'')
-			// 			while (prompt[i] != ' ' && prompt[i])
-			// 				i++;
-			// 	if (prompt[i + 1] == '\'')
-			// 		{
-			// 			i++;
-			// 			flag = prompt[i];
-			// 			while (prompt[i] && prompt[i] != '\'')
-			// 				i++;		
-			// 		}
-			// }
-			if (prompt[i])
-				i++;
-			// if (flag)
-			// 	return (-42);
 		}
 	}
-	if (flag)
-		return (-42);
 	return (count);
 }
 
-void	rucursive(char *prompt, int64_t i, int64_t flag)
+int64_t	recursive(char *prompt, int64_t *i, int64_t *flag)
 {
-	while (prompt[i] && prompt[i] != '\'')
-				i++;
-		if (prompt[i] == '\'')
+	while (prompt[*i] && prompt[*i] != '\'')
+		(*i)++;
+	if (prompt[*i] == '\'')
+	{
+		*flag = 0;
+		if (prompt[*i] && prompt[*i + 1] != ' ' && prompt[*i + 1] != '\'')
+			while (prompt[*i] != ' ' && prompt[*i])
+				(*i)++;
+		if (prompt[*i] && prompt[*i + 1] == '\'')
 		{
-			flag = 0;
-			if (prompt[i] && prompt[i + 1] != ' ' && prompt[i + 1] != '\'')
-					while (prompt[i] != ' ' && prompt[i])
-						i++;
-			if (prompt[i] && prompt[i + 1] == '\'')
-				{
-					i++;
-					flag = prompt[i];
-					i++;
-					rucursive(prompt, i, flag);
-				}	
+			(*i)++;
+			*flag = prompt[*i];
+			(*i)++;
+			return (recursive(prompt, i, flag));
 		}
+	}
+	else
+		return (-42);
+	return (0);
 }
 
+// int64_t	len(char *p)
+// {
+// 	int64_t	i;
+// 	int64_t	c;
+// 	int64_t	f;
+
+// 	i = ;
+// }
 
 #include <stdio.h>
 
 int	main()
 {
-	printf("%lld\n" ,separated_list_len("'aa''a a'"));
-	// printf("%lld\n" ,separated_list_len("'aa''a a'a ' a' 'a"));
+	// printf("%lld\n" ,separated_list_len("'aa''a a'"));
+	printf("%lld\n" , separated_list_len("'aa''a a'a ' a' 'a'"));
 	// printf("%lld\n" ,separated_list_len("at at    a''  ''at"));
 }
