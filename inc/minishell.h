1f77f7ee6065fa40ef91ea5b5b208ed9ca9b6bf4
asdsec <asdsec@proton.me>
2024-05-21 14:11:46 +0300

    Complete redirect

--- inc/minishell.h
+++ inc/minishell.h
@@ -1,11 +1,11 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <readline/history.h>
+# include <readline/readline.h>
 # include <stdbool.h>
 # include <stdint.h>
 # include <stdio.h>
-# include <readline/readline.h>
-# include <readline/history.h>
 
 # define SUCCESS 0
 # define FAILURE -1
@@ -47,12 +47,12 @@ typedef struct s_token_append_meta_data
 
 typedef struct s_token_separation_meta_data
 {
-	t_token	**token_arr;
-	t_token	*iter;
-	t_token	*tmp;
-	t_token	*tmp_root;
-	int64_t	i;
-}			t_token_separation_meta_data;
+	t_token			**token_arr;
+	t_token			*iter;
+	t_token			*tmp;
+	t_token			*tmp_root;
+	int64_t			i;
+}					t_token_separation_meta_data;
 
 typedef struct s_state
 {
@@ -63,39 +63,39 @@ typedef struct s_state
 
 typedef struct s_cmd
 {
-	char	*cmd;
-	char	**argv;
-	int		in;
-	int		out;
-}				t_cmd;
+	char			*cmd;
+	char			**argv;
+	int				in;
+	int				out;
+	int				*heredoc;
+}					t_cmd;
 
 typedef struct s_error
 {
-	int		errnum;
-	char	*message;
-	bool	fatal;
-}				t_error;
+	int				errnum;
+	char			*message;
+	bool			fatal;
+}					t_error;
 
-void				handle_redl(t_token *token, t_cmd *cmd);
-int64_t			ft_strlen(const char *s);
+int64_t				ft_strlen(const char *s);
 char				*ft_strdup(const char *src);
 char				*ft_substr(char const *s, unsigned int start, int64_t len);
-int64_t			ft_strlcpy(char *dst, const char *src, int64_t dst_size);
+int64_t				ft_strlcpy(char *dst, const char *src, int64_t dst_size);
 char				*ft_itoa(int64_t n);
-int64_t			pipe_exec(t_token **token_arr, t_state *state);
-t_token			*token_new(char *data, t_token_type type);
-t_token			*token_add_last(t_token *token, t_token *new);
+int64_t				pipe_exec(t_token **token_arr, t_state *state);
+t_token				*token_new(char *data, t_token_type type);
+t_token				*token_add_last(t_token *token, t_token *new);
 void				token_add_next(t_token *token, t_token *new);
 void				token_add_prev(t_token **token, t_token *new);
 void				token_dispose(t_token **token);
 void				token_dispose_all(t_token **token);
-t_token			**token_separate_by_pipe(t_token *token);
+t_token				**token_separate_by_pipe(t_token *token);
 
-int64_t			create_separated_node(t_token **root, char *prompt,
+int64_t				create_separated_node(t_token **root, char *prompt,
 						int64_t start, int64_t i);
 t_token				*separate_prompt_by_space(char *prompt);
 int64_t				pass_quoted_str(char *p, int64_t *oi);
-t_token_type	get_meta_type(char *data, int64_t i);
+t_token_type		get_meta_type(char *data, int64_t i);
 void				token_append_meta_pipe(t_token **token);
 void				token_append_meta_redl(t_token **token);
 void				token_append_meta_redll(t_token **token);
@@ -114,25 +114,29 @@ t_token				*token_get_root(t_token *node);
 bool				token_is_just_meta(t_token **token);
 t_token				*extract_meta_chars(t_token **root);
 bool				has_syntax_errs(t_token **root);
-void				extract_dollar_key_values(char **data, t_state *state, bool *has_dollar);
-bool				token_separation_meta_data_init(t_token_separation_meta_data *md, t_token *token);
-void	token_old_del(t_token **tmp, t_token *root);
-bool	is_valid_dollar(char *data, int64_t i);
-bool	is_digit(char c);
-bool	is_alpha(char c);
-bool	is_alnum_underscore(char c);
-void	handle_dollar(t_token **root, t_state *state);
-int64_t	handle_special_dollar(char **data, int64_t start, int64_t i,
-		t_state *state);
-void	handle_number_dollar(char **data, int64_t start, int64_t i);
-int64_t	handle_regular_dollar(char **data, int64_t start, int64_t i);
-char	*create_data_from_dollar(char *data, char *value, int64_t start,
-		int64_t index);
-void	extract_dollar_key_values(char **data, t_state *state, bool *has_dollar);
-char	*get_dollar_value(char *key);
+void				extract_dollar_key_values(char **data, t_state *state,
+						bool *has_dollar);
+bool				token_separation_meta_data_init(t_token_separation_meta_data *md,
+						t_token *token);
+void				token_old_del(t_token **tmp, t_token *root);
+bool				is_valid_dollar(char *data, int64_t i);
+bool				is_digit(char c);
+bool				is_alpha(char c);
+bool				is_alnum_underscore(char c);
+void				handle_dollar(t_token **root, t_state *state);
+int64_t				handle_special_dollar(char **data, int64_t start, int64_t i,
+						t_state *state);
+void				handle_number_dollar(char **data, int64_t start, int64_t i);
+int64_t				handle_regular_dollar(char **data, int64_t start,
+						int64_t i);
+char				*create_data_from_dollar(char *data, char *value,
+						int64_t start, int64_t index);
+void				extract_dollar_key_values(char **data, t_state *state,
+						bool *has_dollar);
+char				*get_dollar_value(char *key);
 
-int64_t	ft_strcmp(char *s1, char *s2);
-int64_t					ft_strncmp(const char *s1, const char *s2, int64_t n);
+int64_t				ft_strcmp(char *s1, char *s2);
+int64_t				ft_strncmp(const char *s1, const char *s2, int64_t n);
 void				token_insert_dollar_nodes(t_token **token);
 
 void				token_dispose(t_token **token);
@@ -140,28 +144,33 @@ void				token_dispose_all(t_token **token);
 int64_t				token_count_pipe(t_token *token);
 t_token				**token_separate_by_pipe(t_token *token);
 t_token				*token_get_last(t_token *node);
-void	assign_token_types(t_token *token);
-void	assign_token_arr_types(t_token **token_arr);
+void				assign_token_types(t_token *token);
+void				assign_token_arr_types(t_token **token_arr);
 
-void	error_print(t_error *err);
-t_token	**lexer(char *prompt, t_state *state);
+void				error_print(t_error *err);
+t_token				**lexer(char *prompt, t_state *state);
 
-void	handle_unnecessary_quotes(t_token *root);
-char	**ft_split(char const *str, char c);
-int64_t	token_arr_len(t_token **token_arr);
-char	*find_path(char *command, char **env);
-char	*ft_strjoin(char const *s1, char const *s2, bool flag_free);
-char	*token_join_arg_str(t_token *token);
-int64_t	fork_init(int (*fd)[2], int64_t arr_len, t_token **token_arr, t_state *state, t_cmd *cmd);
-int64_t	pipe_single_exec(t_token *token, t_state *state, t_cmd *cmd);
-int64_t	pipe_init(int (*fd)[2], int64_t pipe_count);
-void	set_red_file_fds(t_token *token, t_cmd *cmd);
-void	set_cmd_arg_and_path(t_token *token, t_state *state, t_cmd	*cmd);
-void	handle_redl(t_token	*token, t_cmd *cmd);
-void	handle_redr(t_token	*token, t_cmd *cmd);
-void	handle_redrr(t_token	*token, t_cmd *cmd);
-void	handle_redll(t_token *token, t_cmd *cmd);
+void				handle_unnecessary_quotes(t_token *root);
+char				**ft_split(char const *str, char c);
+int64_t				token_arr_len(t_token **token_arr);
+char				*find_path(char *command, char **env);
+char				*ft_strjoin(char const *s1, char const *s2, bool flag_free);
+char				*token_join_arg_str(t_token *token);
+int64_t				fork_init(int (*fd)[2], int64_t arr_len,
+						t_token **token_arr, t_state *state, t_cmd *cmd);
+int64_t				pipe_single_exec(t_token *token, t_state *state,
+						t_cmd *cmd);
+int64_t				pipe_init(int (*fd)[2], int64_t pipe_count);
+void				set_red_file_fds(t_token *token, t_cmd *cmd);
+void				set_cmd_arg_and_path(t_token *token, t_state *state,
+						t_cmd *cmd);
+void				handle_redl(t_token *token, t_cmd *cmd,
+						bool has_last_heredoc);
+void				handle_redr(t_token *token, t_cmd *cmd);
+void				handle_redrr(t_token *token, t_cmd *cmd);
+void				handle_redll(t_token *token, t_cmd *cmd, int64_t i);
 
-void	print_err(const char *file, int err_flag);
+void				print_err(const char *file, int err_flag);
+void				set_heredoc_fds(t_token *token, t_cmd *cmd, int64_t i);
 
 #endif
--- src/pipe.c
+++ src/pipe.c
@@ -29,8 +29,11 @@ int64_t	pipe_single_exec(t_token *token, t_state *state, t_cmd *cmd)
 
 	if (!token || !state || !cmd)
 		return (FAILURE);
+	set_heredoc_fds(token, cmd, 0);
 	set_red_file_fds(token, cmd);
 	set_cmd_arg_and_path(token, state, cmd);
+	if (cmd->in == -2)
+		cmd->in = cmd->heredoc[0];
 	if (!is_built_in(token))
 	{
 		pid = fork();
@@ -40,7 +43,6 @@ int64_t	pipe_single_exec(t_token *token, t_state *state, t_cmd *cmd)
 		{
 			if (cmd->in != -2)
 				dup2(cmd->in, STDIN_FILENO);
-			cmd->cmd = "/bin/`cat";
 			if (execve(cmd->cmd, cmd->argv, state->env) == -1)
 				exit(1); // todo(sademir): handle error case
 		}
@@ -91,6 +93,8 @@ static void	handle_child_process(t_token **token_arr, t_state *state, int i,
 		}
 		j++;
 	}
+	if (cmd->in == -2)
+		cmd->in = cmd->heredoc[i];
 	if (i != 0)
 	{
 		close(fd[i - 1][1]);
@@ -127,11 +131,9 @@ int64_t	fork_init(int (*fd)[2], int64_t arr_len, t_token **token_arr,
 	i = 0;
 	if (!fd || arr_len < 0)
 		return (FAILURE);
-
-
-	handle_redll(token_arr[i], cmd);
 	while (i < arr_len)
 	{
+		set_heredoc_fds(token_arr[i], cmd, i);
 		pid = fork();
 		if (pid == -1)
 			return (FAILURE); // todo(kkarakus): handle close;
@@ -153,34 +155,50 @@ int64_t	fork_init(int (*fd)[2], int64_t arr_len, t_token **token_arr,
 		wait(NULL);
 		i++;
 	}
-	printf("waited\n");
+	return (SUCCESS);
+}
+
+int64_t	cmd_init(t_cmd *cmd, int64_t arr_len)
+{
+	int32_t	i;
+
+	cmd->argv = NULL;
+	cmd->cmd = NULL;
+	cmd->in = -2;
+	cmd->out = -2;
+	cmd->heredoc = (int *)malloc(sizeof(int) * arr_len);
+	if (!cmd->heredoc)
+		return (FAILURE);
+	i = 0;
+	while (i < arr_len)
+	{
+		cmd->heredoc[i] = -2;
+		i++;
+	}
 	return (SUCCESS);
 }
 
 int64_t	pipe_exec(t_token **token_arr, t_state *state)
 {
 	int64_t	arr_len;
-	t_cmd	cmd;
+	t_cmd		cmd;
+	int		(*fd)[2];
 
-	int(*fd)[2];
-	// todo(sademir): check in out values
-	cmd.argv = NULL;
-	cmd.cmd = NULL;
-	cmd.in = -2;
-	cmd.out = -2;
 	if (!token_arr || !state)
 		return (FAILURE);
 	arr_len = token_arr_len(token_arr);
 	if (arr_len < 1)
 		return (FAILURE);
+	if (cmd_init(&cmd, arr_len) != SUCCESS)
+		return (FAILURE);
 	if (arr_len == 1)
 		return (pipe_single_exec(token_arr[0], state, &cmd));
 	fd = (int(*)[2])malloc(sizeof(int[2]) * (arr_len - 1));
 	if (!fd)
-		return (FAILURE);
+		return (free(cmd.heredoc), FAILURE);
 	if (pipe_init(fd, arr_len - 1) != SUCCESS)
-		return (FAILURE);
+		return (free(fd), free(cmd.heredoc), FAILURE);
 	if (fork_init(fd, arr_len, token_arr, state, &cmd) != SUCCESS)
-		return (FAILURE);
+		return (free(fd), free(cmd.heredoc), FAILURE);
 	return (SUCCESS);
 }
