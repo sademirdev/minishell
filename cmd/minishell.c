8fb906c058def9a3832b25af57bc78d546e7c812
Samet Demir <sademir@k1m39s02.42istanbul.com.tr>
2024-07-02 21:01:22 +0300

    exit code status fixed

--- cmd/minishell.c
+++ cmd/minishell.c
@@ -87,5 +87,5 @@ int	main(int argc, char **argv, char **env)
 			continue ;
 		run_executor(state);
 	}
-	return (state_dispose(&state), 0);
+	return (state_dispose(&state), state->status);
 }
--- src/exit.c
+++ src/exit.c
@@ -9,22 +9,23 @@ int	exec_exit(t_state *state, t_token *token)
 {
 	int	exit_code;
 
+	exit_code = 0;
 	if (!token)
 		print_fatal_err("invalid argument\n", 2);
 	if (token && token->next && token->next->next)
 		return (print_exec_err(state, token, 1, ERR_TOO_MANY_ARG));
 	if (token->next && !ft_is_digit(token->next->data))
-		return (print_exec_err(state, token, 255, ERR_NUMERIC_ARG_REQUIRED));
+		return (print_exec_err(state, token, 255, ERR_NUMERIC_ARG_REQUIRED), exit(255), FAILURE);
 	if (token->next)
 	{
 		exit_code = ft_atoi(token->next->data);
 		if (exit_code < 0)
 			return (print_exec_err(state, token, \
-				(int)(256 + (exit_code % 256)), ENO_OTHER));
+				(int)(256 + (exit_code % 256)), ENO_OTHER), exit(exit_code), FAILURE);
 		return (print_exec_err(state, token, \
-			(int)(exit_code % 256), ENO_OTHER));
+			(int)(exit_code % 256), ENO_OTHER), exit(exit_code), FAILURE);
 	}
-	return (SUCCESS);
+	return (exit(exit_code), SUCCESS);
 }
 
 static bool	ft_is_digit(char *c)
